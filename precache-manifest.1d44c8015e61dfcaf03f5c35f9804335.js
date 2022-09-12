47e0c3830c97b835016bece3c6384daf0730875d
tiga <2517789608@qq.com>
2022-09-12 17:23:44 +0800

    deploy

---
+++ precache-manifest.1d44c8015e61dfcaf03f5c35f9804335.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d8778c06802e6a44c3",
+    "url": "/Morney-website/css/app.24ffce43.css"
+  },
+  {
+    "revision": "01ae2c8ecff1b27ce763d84563b93e76",
+    "url": "/Morney-website/index.html"
+  },
+  {
+    "revision": "b4d8778c06802e6a44c3",
+    "url": "/Morney-website/js/app.9bf8c629.js"
+  },
+  {
+    "revision": "c7547d99efe91bbff474",
+    "url": "/Morney-website/js/chunk-vendors.7012424a.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/Morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Morney-website/robots.txt"
+  }
+]);
